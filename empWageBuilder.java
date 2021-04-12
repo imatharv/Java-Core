9a5c8bb75eb4f7360fad878844776b365b75424f
Atharva Joshi <iamatharvaj@google.com>
2021-04-12 12:39:31 +0530

    Added: Sulution for employee wage builder problem use-case-1

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,5 +1,6 @@
 public class empWageBuilder {
 	public static void main(String[] args) {
+		//use-case-1
 		int isFullTime = 1;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == isFullTime)
